1a056d9b19137d19845eb08804690db2ebbaf613
Remix Workspace <>
2022-10-08 17:15:33 +0530

    view/pureFunction

--- Calculator.sol
+++ Calculator.sol
@@ -4,11 +4,18 @@ pragma solidity ^0.8.7;
 
 contract Calculator
 {
+    bool _isPublic= true;
+    
     function add(uint256 num1, uint num2) public returns(uint256)
     //not a write interaction, hence it should be pure or view function.
     // view means reading state of blockchain. 
     //here, pure, since not even reading the blockchain
     {
         return num1+num2;
+    }
+
+    function isPublic() public view returns (bool)
+    {
+        return _isPublic;
     }
 }
